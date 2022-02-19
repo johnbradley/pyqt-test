8c958aa5e448402802ef8b2efcc8efb778e54fd1
johnbradley <johnbradley2008@gmail.com>
2022-02-19 13:15:25 -0500

    Add setup.py create a module

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='pyqt-test',
+    version='0.1dev',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=''
+)
