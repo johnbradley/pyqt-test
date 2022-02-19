73160094011d9d19db6b5fc57e4ab81d2130113a
johnbradley <johnbradley2008@gmail.com>
2022-02-19 13:16:24 -0500

    Add package to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='pyqt-test',
     version='0.1dev',
+    packages=['pyqt_test',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=''
 )
